41046fe513f6590b98a789475ca8a13433579e18
staakaar <vancyroletaka@gmail.com>
2024-04-06 13:17:14 +0900

    chore: minimaize isolate rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-// 標準ライブラリを無効
-#![no_std]
+#![no_std] // 標準ライブラリを無効
+#![no_main] // すべてのRustレベルのエントリポイントを無効
 
 use core::panic::PanicInfo;
 
 
-fn main() {}
+#[no_mangle] // この関数の名前修飾をしない
+pub extern "C" fn _start() -> ! {
+    // リンカはデフォルトで_startという名前を返すため
+    loop {
+        
+    }
+}
 
+// パニック時にコール
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
