a6a579724d3bcb48b99a5682c62e4514a80d019c
staakaar <vancyroletaka@gmail.com>
2024-04-07 01:04:11 +0900

    chore: vga text buffer

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,23 @@
 
 use core::panic::PanicInfo;
 
+mod vga_buffer;
+
+static HELLO: &[u8] = b"Hello World";
 
 #[no_mangle] // この関数の名前修飾をしない
 pub extern "C" fn _start() -> ! {
+    let vga_buffer = 0xb8000 as *mut u8;
     // リンカはデフォルトで_startという名前を返すため
-    loop {
-        
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            *vga_buffer.offset(i as isize * 2) = byte;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
     }
+
+    vga_buffer::print_something();
+    loop {}
 }
 
 // パニック時にコール
