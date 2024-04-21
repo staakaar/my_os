f048a4f0447df7b0dc2aebbca5c1f90f6decd153
staakaar <vancyroletaka@gmail.com>
2024-04-22 00:33:59 +0900

    chore: offsetPageTable

--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,30 @@
 
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
-use my_os::println;
+use my_os::{memory::{self, BootInfoFrameAllocator}, println};
 use x86_64::structures::paging::PageTable;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // use my_os::memory::active_level_4_table;
-    use x86_64::VirtAddr;
+    use x86_64::{VirtAddr, structures::paging::Translate};
     use my_os::memory::translate_addr;
 
     println!("Hello World{}", "!");
     my_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    // 未使用のページをまっぷする
+    let page = Page::containing_address(VirtAddr::new(0));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // 新しいマッピングを使って、文字列New!を画面に書き出す
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
     // let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
     // for (i, entry) in l4_table.iter().enumerate() {
@@ -48,7 +58,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        let phys = mapper.translate_addr(virt);
         println!("{:?} -> {:?}", virt, phys);
     }
 
--- src/memory.rs
+++ src/memory.rs
@@ -2,9 +2,60 @@ use x86_64::{
     structures::paging::PageTable,
     VirtAddr,
 };
-use x86_64::PhysAddr;
+use x86_64::{PhysAddr, structures::paging::{Page, PhysFrame, Mapper, Size4KiB, FrameAllocator}};
+use x86_64::structures::paging::OffsetPageTable;
+use bootloader::bootinfo::{MemoryMap, MemoryRegionType};
 
-pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
+pub struct EmptyFrameAllocator;
+
+pub struct BootInfoFrameAllocator {
+    memory_map: &'static MemoryMap,
+    next: usize,
+}
+
+impl BootInfoFrameAllocator {
+    pub unsafe fn init(memory_map: &'static MemoryMap) -> Self {
+        BootInfoFrameAllocator {
+            memory_map,
+            next: 0,
+        }
+    }
+
+    // メモリマップによって指定されたunsableなフレームのイテレーターを返却
+    fn usable_frames(&self) -> impl Iterator<Item = PhysFrame> {
+        // メモリマップからusableな領域を得る
+        let regions = self.memory_map.iter();
+        let usable_regions = regions.filter(|r| r.region_type == MemoryRegionType::Usable);
+
+        // それぞれの領域をアドレス範囲にmapで変換する
+        let addr_ranges = usable_regions.map(|r| r.range.start_addr()..r.range.end_addr());
+        // フレームの開始アドレスのイテレーターへと変換する
+        let frame_addresses = addr_ranges.flat_map(|r| r.step_by(4096));
+        // 開始アドレスから`PhysFrame`型を作る
+        frame_addresses.map(|addr| PhysFrame::containing_address(PhysAddr::new(addr)))
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for BootInfoFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        let frame = self.usable_frames().nth(self.next);
+        self.next += 1;
+        frame
+    }
+}
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
+
+pub unsafe fn init(physical_memory_offset: VirtAddr) -> OffsetPageTable<'static> {
+    let level_4_table = active_level_4_table(physical_memory_offset);
+    OffsetPageTable::new(level_4_table, physical_memory_offset)
+}
+
+unsafe fn active_level_4_table(physical_memory_offset: VirtAddr) -> &'static mut PageTable {
     use x86_64::registers::control::Cr3;
 
     let (level_4_table_frame, _) = Cr3::read();
@@ -43,4 +94,15 @@ fn translate_addr_inner(addr: VirtAddr, physical_memory_offset: VirtAddr) -> Opt
     }
 
     Some(frame.start_address() + u64::from(addr.page_offset()))
+}
+
+pub fn create_example_mapping(page: Page, mapper: &mut OffsetPageTable, frame_allocator: &mut impl FrameAllocator<Size4KiB>) {
+    use x86_64::structures::paging::PageTableFlags as Flags;
+
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = Flags::PRESENT | Flags::WRITABLE;
+
+    let map_to_result = unsafe { mapper.map_to(page, frame, flags, frame_allocator) };
+
+    map_to_result.expect("map to failed").flush();
 }
